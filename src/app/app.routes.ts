63ea2355496258e916053d756697af34151733c2
Alessandro De Pietri Tonelli <adepietri.it@gmail.com>
2024-09-10 17:28:53 +0200

    first-commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './auth/layouts/auth-layout/auth-layout.component';
+import { LoginPageComponent } from './auth/pages/login-page/login-page.component';
+import { RegisterPageComponent } from './auth/pages/register-page/register-page.component';
+import { DashboardLayoutComponent } from './dashboard/layouts/dashboard-layout/dashboard-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthLayoutComponent,
+    children: [
+      { path: 'login', component: LoginPageComponent },
+      { path: 'register', component: RegisterPageComponent },
+      { path: '**', redirectTo: 'login' },
+    ]
+  },
+  {
+    path: 'dashboard',
+    component: DashboardLayoutComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'auth'
+  }
+
+];
